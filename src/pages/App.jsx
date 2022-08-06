23355c505118d9abb8bb253578c80418ccb12a07
BO55man <ginkgo1003@gmail.com>
2022-08-06 17:36:33 +0900

    final final

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -26,8 +26,8 @@ function App() {
     <main>
       <div className="content">
         <h2 className="content__title"> 지식에 자유를 </h2>
-        <div className="content__title--sub1"> 책 내용이 웹에서 바로 검색됩니다. </div>
-        <div className="content__title--sub2"> 웹과 시멘틱 태그에 기반한 검색가능한 책 솔루션. </div>
+        <div className="content__title--sub1"> 검색 한번으로 간편하게 접근하고, <br className="line-change"/> 직관적으로 활용하세요.</div>
+        <div className="content__title--sub2"> 책 내용이 웹에서 바로 검색됩니다. </div>
         <p> 지금 사전신청 하세요. </p>
         <form onSubmit={onSubmit} className="input">
           <input 
@@ -40,7 +40,7 @@ function App() {
       <div className="content__sub">
         <div className="content__info">
           <h3> 책을 웹에서 바로 만나보세요. </h3>
-          <p> PDF, 전자책처럼 디바이스에 일일히 저장할 필요 없습니다. 웹 브라우저에서 빠르고 간편하게 열람하세요. </p>
+          <p> PDF, 전자책처럼 디바이스에 일일히 저장할 필요 없습니다. 웹 브라우저에서 검색 한번으로 원하는 부분만 빠르고 간편하게 열람하세요. </p>
         </div>
         <video
         className="content__img"
@@ -60,15 +60,15 @@ function App() {
         muted
         loop></video>
         <div className="content__info">
-          <h3> 인상적이었던 부분을 북마크에 저장하고<br/> 언제든 다시 찾아보세요. </h3>
-          <p> 직관적으로 저장하고 주변에 공유하세요. </p>
+          <h3> 인상적이었던 부분을 <br/> 북마크에 저장하고 <br className="line-change"/> 언제든 다시 찾아보세요. </h3>
+          <p> 직관적으로 저장하고 주변에 공유까지 간단하게. </p>
         </div>
       </div>
       <div className="content__divider"></div>
       <div className="content__sub">
         <div className="content__info">
           <h3> 책의 유비쿼터스를 경험하세요. </h3>
-          <p> 인터넷만 연결된다면 OK입니다. 언제 어디서든, 다양한 검색엔진과 디바이스에서 로제타와 함께하세요. </p>
+          <p> 인터넷만 연결된다면 OK입니다. 언제 어디서든, 다양한 디바이스와 브라우저에서 로제타와 함께하세요. </p>
         </div>
         <video
         className="content__img"
@@ -87,7 +87,7 @@ function App() {
         </div>
       </div>
       <div className="content">
-        <div className="content__title--sub1"> 지금 사전신청하고<br/> 출시 전 알림을 받아보세요. </div>
+        <div className="content__title--sub1"> 지금 사전신청하고<br className="line-change"/> 출시 전 알림을 받아보세요. </div>
         <div className="content__additional"> 제공해주신 이메일 정보는 알림 외 다른 목적으로 사용되지 않습니다. </div>
         <form onSubmit={onSubmit} className="input">
             <input 
