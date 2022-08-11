e8d88c9765b82e2be1c2eda2191e77a3caf5b449
BO55man <ginkgo1003@gmail.com>
2022-08-11 12:01:24 +0900

    2nd

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,19 +6,27 @@ import { dbService } from '../firebase';
 
 function App() {
 
-  const [preorder, setPreOrder] = useState("")
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    await dbService.collection("preorder").add({
-      preorder,
-      createAt : Date.now(),
-    });
-    setPreOrder("");
-  };
+  const [preOrder, setPreOrder] = useState("");
+  const [disabled, setDisabled] = useState(false);
   const onChange = (event) => {
     const { target: {value} } = event;
     setPreOrder(value);
   };
+  const onSubmit = async (event) => {
+    setDisabled(true);
+    event.preventDefault();    
+    if (preOrder.length < 3) {
+      alert("올바를 이메일을 입력해주세요.");
+    } else {
+      await dbService.collection("preorder").add({
+        preOrder
+      });
+      setPreOrder("");
+      await new Promise((r) => setTimeout(r, 1000));
+      alert('이메일이 제출되었습니다.');
+    }
+    setDisabled(false);
+  }
 
   return (
     <>
@@ -30,10 +38,10 @@ function App() {
         <div className="content__title--sub2"> 이메일을 입력하시면 서비스 구독권을 보내 드립니다. </div>
         <form onSubmit={onSubmit} className="input">
           <input 
-            value={preorder} 
+            value={preOrder} 
             onChange={onChange} 
             className="input__email" type="email" placeholder="이메일 주소" name="email" maxLength={30}/>
-          <input className="input__submit" type="submit" value="서비스 구독권 받기"/>
+          <button type="submit" className="input__submit">서비스 구독권 받기</button>
         </form>
       </div>
       <div className="content__sub">
@@ -81,11 +89,11 @@ function App() {
         <div className="content__title--sub"> 지금 이메일을 입력하시고<br className="line-change"/> 서비스 구독권을 받아보세요. </div>
         <div className="content__title--sub2"> 제공해주신 이메일 정보는 <br className="line-change"/> 알림 외 다른 목적으로 사용되지 않습니다. </div>
         <form onSubmit={onSubmit} className="input">
-            <input 
-              value={preorder} 
-              onChange={onChange} 
-              className="input__email" type="email" placeholder="이메일 주소" name="email" maxLength={30}/>
-            <input className="input__submit" type="submit" value="서비스 구독권 받기"/>
+          <input 
+            value={preOrder} 
+            onChange={onChange} 
+            className="input__email" type="email" placeholder="이메일 주소" name="email" maxLength={30}/>
+          <button type="submit" className="input__submit">서비스 구독권 받기</button>
         </form>
       </div>
     </main>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,33 +4,41 @@ import { dbService } from '../firebase';
 
 function Contact() {
 
-  const [preorder, setPreOrder] = useState("")
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    await dbService.collection("preorder").add({
-      preorder,
-      createAt : Date.now(),
-    });
-    setPreOrder("");
-  };
+  const [preOrder, setPreOrder] = useState("");
+  const [disabled, setDisabled] = useState(false);
   const onChange = (event) => {
     const { target: {value} } = event;
     setPreOrder(value);
   };
-
+  const onSubmit = async (event) => {
+    setDisabled(true);
+    event.preventDefault();    
+    if (preOrder.length < 3) {
+      alert("올바를 이메일을 입력해주세요.");
+    } else {
+      await dbService.collection("preorder").add({
+        preOrder
+      });
+      setPreOrder("");
+      await new Promise((r) => setTimeout(r, 1000));
+      alert('이메일이 제출되었습니다.');
+    }
+    setDisabled(false);
+  }
+  
   return(
     <>
     <Header/>
     <main>
     <div className="content">
-      <p> 지금 사전신청 하세요. </p>
-      <form onSubmit={onSubmit} className="input">
-        <input 
-          value={preorder} 
-          onChange={onChange} 
-          className="input__email" type="email" placeholder="이메일 주소" name="email" maxLength={30}/>
-        <input className="input__submit" type="submit" value="사전신청 하기"/>
-      </form>
+    <div className="content__title--sub2"> 이메일을 입력하시면 서비스 구독권을 보내 드립니다. </div>
+        <form onSubmit={onSubmit} className="input">
+          <input 
+            value={preOrder} 
+            onChange={onChange} 
+            className="input__email" type="email" placeholder="이메일 주소" name="email" maxLength={30}/>
+          <button type="submit" className="input__submit">서비스 구독권 받기</button>
+        </form>
     </div>
     </main>
     <Footer/>
